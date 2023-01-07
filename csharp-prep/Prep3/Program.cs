f96197b5ff6489e4ca8d9c2a0cad842f2db47e73
Fex-zi <fexzi87@gmail.com>
2023-01-06 22:21:11 -0700

    added number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        int guess = -1;
+
+        
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            string convert = Console.ReadLine();
+            guess = int.Parse(convert);
+            //guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }
     }
 }
