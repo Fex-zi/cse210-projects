4260f0a6c3d0606a0482729bf9c3db60149f89d3
Fex-zi <fexzi87@gmail.com>
2023-01-31 19:57:15 -0700

    Polymorphism Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,25 @@
 using System;
 
-class Program
+public class Program
 {
     static void Main(string[] args)
     {
-        
+          Square square = new Square("Red", 4);
+          Console.WriteLine("Color: " + square.Color);
+          Console.WriteLine("Area: " + square.GetArea());
+
+          // Repeat the same steps for Rectangle and Circle classes
+
+          List<Shape> shapes = new List<Shape>();
+          shapes.Add(new Square("Green", 5));
+          shapes.Add(new Rectangle("Blue", 4, 6));
+          shapes.Add(new Circle("Yellow", 5));
+
+          foreach (var shape in shapes)
+            {
+                Console.WriteLine("Color: " + shape.Color);
+                Console.WriteLine("Area: " + shape.GetArea());
+             }
+            
     }
 }
--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Threading;
+using System.Threading; 
 
 public class BreathingActivity : Activity
 {
